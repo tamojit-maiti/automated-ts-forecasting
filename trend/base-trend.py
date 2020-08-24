d79d523a1a8aadfed82b537562dabe50c128dda1
Tamojit Maiti <48589313+tamojit-maiti@users.noreply.github.com>
2020-08-24 16:26:01 +0530

    refactored

---
+++ trend/base-trend.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Base Trend Class for all Trend Estimators
+"""
+import numpy as np
+
+class TrendEstimator(TimeSeries):
+    """
+    Defines the Trend Class for the TimeSeries object
+    """
+
+    def __init__(self):
+        """
+        Initializes an instance of the Trend Class
+        """
+        pass
+
+    def
--- trend/ma.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-class Trend(TimeSeries):
-    """
-    Defines the Trend for the TimeSeries object
-    """
